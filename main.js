b3968d69e6313fe00a3eb42435ed02905b016975
Nick Rosso <Nicholasrosso@comcast.net>
2016-12-13 19:38:47 -0600

    Added New Buttons/Cursor
    Not my ideal way of going about multiple upgrades, but it works for now

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
-
-function save(corn,tools,farmHands){
+function save(corn,hoes,plows,tractors,farmHands){
 	var savedValues = {
 		corn: corn,
-		tools: tools,
+		hoes: hoes,
+		plows: plows,
+		tractors: tractors,
 		farmHands: farmHands,
 		
 	};
@@ -12,75 +13,110 @@ function save(corn,tools,farmHands){
 function deleteSave(){
 	localStorage.removeItem("savedValues")
 	corn = 0;
-	tools = 0;
+	hoes = 0;
+	plows = 0;
+	tractors = 0;
 	farmHands = 0;
 };
 
 function load(){
 	var savegame = JSON.parse(localStorage.getItem("savedValues"));
 	if(typeof savegame.corn !== "undefined") corn = savegame.corn;
-	if(typeof savegame.tools !== "undefined") tools = savegame.tools;
+	if(typeof savegame.hoes !== "undefined") hoes = savegame.hoes;
+	if(typeof savegame.plows !== "undefined") plows = savegame.plows;
+	if(typeof savegame.tractors !== "undefined") tractors = savegame.tractors;
 	if(typeof savegame.farmHands !== "undefined") farmHands = savegame.farmHands;
 	
 };
 
 function cornClick(number){
 	corn = corn + number;
-	document.getElementById("totalCorn").innerHTML = corn;
+	document.getElementById("totalCorn").innerHTML = formatNumber(corn);
 
 };
 
+function formatNumber(number){
+	return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+};
 
-function buyItem(){
-	var toolCost = Math.floor(10 * Math.pow(1.5, tools));
+function buyNewItem(name){
+	switch(name){
 
-	if(corn >= toolCost){
-		tools++;
-		corn -= toolCost;
-		document.getElementById('tools').innerHTML = tools;
-		document.getElementById('totalCorn').innerHTML = corn;
+	case 'hoes':
+		var itemCost = Math.floor(10 * Math.pow(1.5, hoes))
+		if(corn >= itemCost){
+			hoes++;
+			corn-=itemCost;
+			document.getElementById('hoes').innerHTML = hoes;
+		};
+		var nextItemCost = Math.floor(10 * Math.pow(1.5, hoes));
+		document.getElementById('hoesCost').innerHTML = formatNumber(nextItemCost);
+		document.getElementById('hoes').innterHTML = hoes;	
+
+	case 'plows':
+		var itemCost = Math.floor(500 * Math.pow(1.5, plows))
+		if(corn >= itemCost){
+			plows++;
+			corn-=itemCost;
+			document.getElementById('plows').innerHTML = plows;
+		};
+		var nextItemCost = Math.floor(500 * Math.pow(1.5, plows));
+		document.getElementById('plowsCost').innerHTML = formatNumber(nextItemCost);
+		document.getElementById('plows').innterHTML = plows;
+	case 'tractors':
+		var itemCost = Math.floor(15000 * Math.pow(1.5, tractors))
+		if(corn >= itemCost){
+			tractors++;
+			corn-=itemCost;
+			document.getElementById('tractors').innerHTML = tractors;
+		};
+		var nextItemCost = Math.floor(15000 * Math.pow(1.5, tractors));
+		document.getElementById('tractorsCost').innerHTML = formatNumber(nextItemCost);
+		document.getElementById('tractors').innterHTML = tractors;
+
+	default:
+		document.getElementById('totalCorn').innerHTML = formatNumber(corn);
 	};
-
-	var nextToolCost = Math.floor(10 * Math.pow(1.5, tools));
-	document.getElementById('toolCost').innerHTML = nextToolCost;
-	document.getElementById('tools').innterHTML = tools;
+	document.getElementById('totalCorn').innerHTML = formatNumber(corn);
 };
-
 
 function buyFarmHand(){
-	var farmHandCost = Math.floor(100 * Math.pow(2.25, farmHands));
+	var farmHandCost = Math.floor(500 * Math.pow(5.00, farmHands));
 
 	if(corn >= farmHandCost){
 		farmHands++;
 		corn -= farmHandCost;
 		document.getElementById('farmHands').innerHTML = farmHands;
-		document.getElementById('totalCorn').innerHTML = corn;
+		document.getElementById('totalCorn').innerHTML = formatNumber(corn);
 	};
 
-	var nextFarmHandCost = Math.floor(100 * Math.pow(2.25, farmHands));
-	document.getElementById('farmHandCost').innerHTML = nextFarmHandCost;
+	var nextFarmHandCost = Math.floor(500 * Math.pow(5.00, farmHands));
+	document.getElementById('farmHandCost').innerHTML = formatNumber(nextFarmHandCost);
 	document.getElementById('farmHands').innerHTML = farmHands;
 };
 
-function getCornPerSec(tools, farmHands){
+function getCornPerSec(hoes,tractors, plows, farmHands){
 	if(farmHands == 0){
-		cornPerSec = tools;
+		cornPerSec = hoes + plows*10 + tractors*25;
 	}
-	else
-		cornPerSec = tools * (2 * farmHands);
+	else{
+		cornPerSec = (hoes + plows*10 + tractors*25);
+		cornPerSec *= 2 * farmHands;
+	}
 	return cornPerSec;
 };
 
-function updateCornPerSec(){
-	document.getElementById('cornPerSec').innerHTML = cornPerSec;
+function updateGameElements(){
+	document.getElementById('cornPerSec').innerHTML = formatNumber(cornPerSec);
 };
 
 
+
 window.setInterval(function(){
-	cornPerSec = getCornPerSec(tools, farmHands);
+	cornPerSec = getCornPerSec(hoes,tractors, plows, farmHands);
+	updateGameElements();
 	cornClick(cornPerSec);
-	updateCornPerSec();
-	save(corn,tools,farmHands);
+	save(corn,hoes,tractors, plows,farmHands);
 }, 1000);
 
 
